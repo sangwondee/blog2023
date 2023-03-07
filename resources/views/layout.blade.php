269499ea073c6af0957336547e54b68f029d2b28
wichas35 <wichas35@comex.postbox.in.th>
2023-03-07 11:23:52 +0700

    Implementing Authentication

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,34 +1,41 @@
-<!doctype html>
-<html lang="en">
-
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Blog2023')</title>
+    <title>
+        @yield('title', 'Blog')
+    </title>
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
+<body>
 
 <ul class="nav">
     <li><a class="{{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">Home</a></li>
     <li><a class="{{ request()->routeIs('about') ? 'active' : '' }}" href="{{ route('about') }}">About</a></li>
-    <li><a class="{{ request()->routeIs('posts.create') ? 'active' : '' }}"
-            href="{{ route('posts.create') }}">Create</a></li>
+    @auth
+        <li><a class="{{ request()->routeIs('posts.create') ? 'active' : '' }}" href="{{ route('posts.create') }}">
+                Create Post</a>
+        </li>
+        <li><a href="{{ route('logout') }}">Logout</a></li>
+        <li class="username"><p>Logged in as <b>{{ Auth::user()->name }}</b></p></li>
+    @else
+        <li>
+            <a class="{{ request()->routeIs('register') ? 'active' : '' }}" href="{{ route('register') }}">Register</a>
+        </li>
+        <li><a class="{{ request()->routeIs('login') ? 'active' : '' }}" href="{{ route('login') }}">Login</a></li>
+    @endauth
 </ul>
 
 @includeWhen($errors->any(), '_errors')
 
 @if (session('success'))
-<div class="flash-success">
-    {{ session('success') }}
-</div>
+    <div class="flash-success">
+        {{ session('success') }}
+    </div>
 @endif
 
-<body>
-    <div class="main">
-        @yield('content')
-    </div>
-</body>
+<div class="main">
+    @yield('content')
+</div>
 
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
@@ -7,23 +8,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 
+// Auth
+Route::match(['get', 'post'],'/register', [AuthController::class, 'register'])
+    ->name('register')
+    ->middleware('guest');
+
+Route::match(['get', 'post'],'/login', [AuthController::class, 'login'])
+    ->name('login')
+    ->middleware('guest');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+
 // Post
-// แบบใหม่ ทำ crud แบบ Route เดียว
-Route::resource('posts', PostController::class)->except(['index']);
-
-
+Route::resource('posts', PostController::class)->except(['index'])->middleware('auth');
 // แบบเก่า
 // Route::name('posts.')->prefix('posts')->group(function () {
-
 //     Route::get('/posts', [PostController::class, 'index'])->name('index');
-
 //     Route::get('/create', [PostController::class, 'create'])->name('create');
 //     Route::post('/', [PostController::class, 'store'])->name('store');
-
 //     Route::get('/{post}', [PostController::class, 'show'])->name('show');
-
 //     Route::get('/{post}/edit', [PostController::class, 'edit'])->name('edit');
 //     Route::put('/{post}', [PostController::class, 'update'])->name('update');
-
 //     Route::delete('/{post}', [PostController::class, 'destroy'])->name('destroy');
 // });
